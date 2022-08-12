f3526e95e1d297216c1b92f4b1a30e6c31d9fdb4
kevanultroneous <dev21.ultroneous@gmail.com>
2022-08-12 19:41:13 +0530

    commit

--- components/common/Container.jsx
+++ components/common/Container.jsx
@@ -1,4 +1,11 @@
 import styles from "@/styles/components/common/Container.module.css";
-export default function Container({ child }) {
-  return <div>{child}</div>;
+export default function Container({ children, innerstyle, userdefinedclass }) {
+  return (
+    <div
+      className={`${styles.Container} ${userdefinedclass}`}
+      style={innerstyle}
+    >
+      {children}
+    </div>
+  );
 }
