03f00c938393bffcc9e780d021aa6f5b90406678
dev23.ultroneous@gmail.com <dev23.ultroneous@gmail.com>
2022-08-15 20:09:02 +0530

    thank you page changes

--- components/ThankYou/ThankYouBill.jsx
+++ components/ThankYou/ThankYouBill.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ServiceComponent from "./ServiceComponent";
+import { ServiceData } from "utils/serviceData";
+import styles from "@/styles/components/ThankYou/ThankYouBill.module.css";
 
 function ThankYouBill() {
-  return <div>ThankYouBill</div>;
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    setTotalPrice(
+      ServiceData.map((service) => service.servicePrice).reduce((a, b) => a + b)
+    );
+  }, [totalPrice]);
+
+  return (
+    <section className={styles.ThankyouBillMainContainer}>
+      <div className={styles.orderNumber}>
+        <h5>Order: #187</h5>
+      </div>
+      {ServiceData.map((service, ind) => {
+        return (
+          <ServiceComponent
+            key={ind}
+            serviceDesc={service.serviceDesc}
+            serviceName={service.serviceName}
+            serviceImage={service.serviceImage}
+            servicePrice={service.servicePrice}
+          />
+        );
+      })}
+      <div className={styles.totalPrice}>
+        <h5>Order Summary</h5>
+        <hr />
+        <div className={styles.totalAmount}>
+          <h6>Total</h6>
+          <h5>₹ {totalPrice}</h5>
+        </div>
+        <hr />
+      </div>
+    </section>
+  );
 }
 
 export default ThankYouBill;
---
+++ utils/serviceData.js
@@ -0,0 +1,8 @@
+export const ServiceData = [
+  {
+    serviceImage: "/assets/images/split-ac-service.jpg",
+    serviceName: "Split AC - Regular Service",
+    serviceDesc: "Warranty: 3 Months",
+    servicePrice: 549,
+  },
+];
