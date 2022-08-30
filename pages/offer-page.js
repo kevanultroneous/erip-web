3154c37fbd8fb054c051293217b9bc1adf118524
dev23.ultroneous@gmail.com <dev23.ultroneous@gmail.com>
2022-08-30 20:02:00 +0530

    Offer-page-api-integration

--- pages/offer-page.js
+++ pages/offer-page.js
@@ -4,15 +4,14 @@ import { Header } from "@/components/common/Header";
 import Layout from "@/components/common/Layout";
 import Offers from "@/components/OfferPage/Offers";
 import React from "react";
+import styles from "@/styles/components/OfferPage/Offers.module.css";
 
 function offerPage() {
   return (
     <>
       <Layout title={"Offer Page"}>
         <Header />
-        <Container>
-          <Offers />
-        </Container>
+        <Offers />
         <Footer />
       </Layout>
     </>
