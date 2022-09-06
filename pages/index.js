3fba5bebbbe9f80c2bf9936d1e09a13245a4439c
kevanultroneous <dev21.ultroneous@gmail.com>
2022-09-06 16:10:16 +0530

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -92,28 +92,28 @@ export async function getServerSideProps() {
         herosection: !hero_section_home
           ? HomeHerodata
           : hero_section_home.data.length > 0
-          ? hero_section_home
-          : HomeHerodata,
+            ? hero_section_home
+            : HomeHerodata,
         blogdata: !blog_response
           ? NewsandUpdateData
           : blog_response.data.length > 0
-          ? blog_response
-          : NewsandUpdateData,
+            ? blog_response
+            : NewsandUpdateData,
         hometestimonial: !home_testimonial
           ? TestimonialData
           : home_testimonial.data.length > 0
-          ? home_testimonial
-          : TestimonialData,
+            ? home_testimonial
+            : TestimonialData,
         offersection: !offersection_home
           ? OfferBannerhomedata
           : offersection_home.data.length > 0
-          ? offersection_home
-          : OfferBannerhomedata,
+            ? offersection_home
+            : OfferBannerhomedata,
         getyourfix: !getyour_fix
           ? GetYourFixCard
           : getyour_fix.data.length > 0
-          ? getyour_fix
-          : GetYourFixCard,
+            ? getyour_fix
+            : GetYourFixCard,
       },
     },
   };
