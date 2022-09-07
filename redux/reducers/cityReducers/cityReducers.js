5bca2615e581376efa3bb3d2aeca7776beb1fcf5
kevanultroneous <dev21.ultroneous@gmail.com>
2022-09-07 20:03:28 +0530

    commit

--- redux/reducers/cityReducers/cityReducers.js
+++ redux/reducers/cityReducers/cityReducers.js
@@ -3,11 +3,17 @@ import * as city from "../../actions/actionTypes";
 const initialCityState = {
   city: 1,
   error: "",
+  loading: false
 };
 
 export const cityReducer = (state = initialCityState, { type, payload }) => {
   switch (type) {
-    case city.GET_CITY:
+    case city.GET_CITY_START:
+      return {
+        ...state,
+        loading: true
+      };
+    case city.GET_CITY_SUCCESS:
       return {
         ...state,
         err: "",
